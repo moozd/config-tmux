69c7ac3d6e3d326cf830c14684d69c7c6e4f3005
mo <mohammad.mdz72@gmail.com>
2024-06-12 11:53:10 +0200

    fix jump

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,7 +1,9 @@
 # !/usr/bin/env bash
 
 # TODO: read this from a config file
-dirs=$(find ~/.config/ ~/work ~/personal/ ~/personal/tools/ -mindepth 1 -maxdepth 1 -type d -exec test -d {}/.git \; -print | tr " " "\n")
+
+# dirs=$(find ~/.config/ ~/work ~/personal/ ~/personal/tools/ -mindepth 1 -maxdepth 1 -type d -exec test -d {}/.git \; -print | tr " " "\n")
+dirs=$(find ~/.config/ ~/work ~/personal/ ~/personal/tools/ -mindepth 1 -maxdepth 1 -type d | tr " " "\n")
 apps=$(tmuxinator list -n | tail -n +2 | while read i; do echo ":$i"; done)
 jumps=$(
 	find ~/.config/tmux/scripts -type f -name "_*.sh" | while read -r f; do basename $f | sed 's/^_/:/' | sed 's/\.sh$//'; done | tr ' ' '\n'
@@ -27,13 +29,18 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
+command=""
+if [ -d "$selected/.git" ]; then
+	command=$EDITOR
+fi
+
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s $selected_name -c $selected $EDITOR
+	tmux new-session -s $selected_name -c $selected $command
 	exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	tmux new-session -ds $selected_name -c $selected $EDITOR
+	tmux new-session -ds $selected_name -c $selected $command
 fi
 
 tmux switch-client -t $selected_name
