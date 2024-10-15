d9313ae7821f16436d9dbbbd1b686beb200383c5
mo <mohammad.mdz72@gmail.com>
2024-10-15 19:44:09 +0200

    :rocket:

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -2,7 +2,7 @@
 
 # TODO: read this from a config file
 
-dirs=$(find ~/work ~/personal -mindepth 1 -maxdepth 1 -type d | tr " " "\n")
+dirs=$(find ~/work ~/personal ~/.config -mindepth 1 -maxdepth 1 -type d | tr " " "\n")
 apps=$(tmuxinator list -n | tail -n +2 | while read i; do printf ":$i\n"; done)
 jumps=$(
 	find ~/.config/tmux/scripts -type f -name "_*.sh" | while read -r f; do basename $f | sed 's/^_/:/' | sed 's/\.sh$//'; done | tr ' ' '\n'
@@ -28,18 +28,19 @@ fi
 selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
-command=""
-# if [ -d "$selected/.git" ]; then
-# 	command=$EDITOR
+# commands=$(printf "[open] $EDITOR neohub lazygit lazydocker" | tr " " "\n")
+# command=$(printf "$commands" | fzf)
+# if [[ $command == '[open]' ]]; then
+# 	command=""
 # fi
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-	tmux new-session -s $selected_name -c $selected $command
+	tmux new-session -s $selected_name -c $selected
 	exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2>/dev/null; then
-	tmux new-session -ds $selected_name -c $selected $command
+	tmux new-session -ds $selected_name -c $selected
 fi
 
 tmux switch-client -t $selected_name
